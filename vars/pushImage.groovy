1e3c87999c7edd7eee76904a8740d64cab728e70
sivaramanshira <34826837+sivaramanshira@users.noreply.github.com>
2021-09-26 19:20:57 +0530

    Update pushImage.groovy

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -1,5 +1,5 @@
 def call(){
-    sh 'docker login -u admin -p password 40.71.77.234:8082/app-images'
-    sh 'docker tag myapprod 40.71.77.234:8082/app-images/myapprod'
-    sh 'docker push 40.71.77.234:8082/app-images/myapprod'
+    sh 'docker login -u admin -p password 20.102.85.66:8082/docker-local'
+    sh 'docker tag myapprod 20.102.85.66:8082/docker-local/myapprod'
+    sh 'docker push 20.102.85.66:8082/docker-local/myapprod'
 }
