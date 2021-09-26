e2e2bdb5d6a3b3d31d941b36e47734b2dd548dd4
sivaramanshira <34826837+sivaramanshira@users.noreply.github.com>
2021-09-26 20:09:48 +0530

    Update pushImage.groovy

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -2,7 +2,7 @@ def call(){
     sh 'docker login -u admin -p password 20.102.85.66:8082/docker-local'
     sh 'docker tag myapprod 20.102.85.66:8082/docker-local/myapprod'
     sh 'docker push 20.102.85.66:8082/docker-local/myapprod'
-    sh 'sshpass -p ShivaJi123123 ssh -t -t -o StrictHostKeyChecking=no Shiva@20.102.85.66'
+    sh 'sshpass -p ShivaJi123123 ssh -t -t -o StrictHostKeyChecking=no Shiva@20.102.85.66 && pwd'
     sh 'pwd'
     sh 'docker login -u admin -p password 20.102.85.66:8082/docker-local'
     sh 'docker pull 20.102.85.66:8082/docker-local/myapprod'
