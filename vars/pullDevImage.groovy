7c3fefea35d893b387d6b628c2b33778ae8b940f
sivaramanshira <34826837+sivaramanshira@users.noreply.github.com>
2021-09-27 19:26:59 +0530

    Update pullDevImage.groovy

--- vars/pullDevImage.groovy
+++ vars/pullDevImage.groovy
@@ -1,3 +1,3 @@
 def call(){
-    sh 'sshpass -p ShivaJi123123 ssh -t -t -o StrictHostKeyChecking=no Shiva@20.102.85.66 "docker login -u admin -p password 20.102.85.66:8082/docker-local && docker pull 20.102.85.66:8082/docker-local/myappdev"'
+    sh 'sshpass -p ShivaJi123123 ssh -t -t -o StrictHostKeyChecking=no Shiva@20.102.85.66 "docker login -u admin -p password 20.102.85.66:8082/docker-local && docker pull 20.102.85.66:8082/docker-local/myappdev:${SHORT_COMMIT}"'
 }
