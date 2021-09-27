5bdf18182019c4307c0ad95deb2e9045512d8054
sivaramanshira <34826837+sivaramanshira@users.noreply.github.com>
2021-09-27 19:25:42 +0530

    Update deployProdApp.groovy

--- vars/deployProdApp.groovy
+++ vars/deployProdApp.groovy
@@ -1,4 +1,4 @@
 def call(){
-    sh 'sshpass -p ShivaJi123123 ssh -t -t -o StrictHostKeyChecking=no Shiva@10.1.2.4 "docker run -d -p 9092:8080 20.102.85.66:8082/docker-local/myapprod"'
-    sh 'sshpass -p ShivaJi123123 ssh -t -t -o StrictHostKeyChecking=no Shiva@10.1.2.5 "docker run -d -p 9092:8080 20.102.85.66:8082/docker-local/myapprod"'
+    sh 'sshpass -p ShivaJi123123 ssh -t -t -o StrictHostKeyChecking=no Shiva@10.1.2.4 "docker run -d -p 9092:8080 20.102.85.66:8082/docker-local/myapprod:${SHORT_COMMIT}"'
+    sh 'sshpass -p ShivaJi123123 ssh -t -t -o StrictHostKeyChecking=no Shiva@10.1.2.5 "docker run -d -p 9092:8080 20.102.85.66:8082/docker-local/myapprod:${SHORT_COMMIT}"'
 }
